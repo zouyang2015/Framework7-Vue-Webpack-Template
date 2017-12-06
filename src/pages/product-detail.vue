f88c850556b0c478e0d9362c4dc0373aa56f9a45
zouyang <zouyang@baokeyun.com>
2017-12-06 18:02:01 +0800

    detail

--- src/pages/product-detail.vue
+++ src/pages/product-detail.vue
@@ -1,12 +1,18 @@
 <template>
   <f7-page>
-    <f7-navbar title="About" back-link="Back" sliding></f7-navbar>
-    <f7-block inner>
-      <p>Here is About page!</p>
-      <p>You can go <f7-link back>back</f7-link>.</p>
-      <p>Mauris posuere sit amet metus id venenatis. Ut ante dolor, tempor nec commodo rutrum, varius at sem. Nullam ac nisi non neque ornare pretium. Nulla mauris mauris, consequat et elementum sit amet, egestas sed orci. In hac habitasse platea dictumst.</p>
-      <p>Fusce eros lectus, accumsan eget mi vel, iaculis tincidunt felis. Nulla tincidunt pharetra sagittis. Fusce in felis eros. Nulla sit amet aliquam lorem, et gravida ipsum. Mauris consectetur nisl non sollicitudin tristique. Praesent vitae metus ac quam rhoncus mattis vel et nisi. Aenean aliquet, felis quis dignissim iaculis, lectus quam tincidunt ligula, et venenatis turpis risus sed lorem. Morbi eu metus elit. Ut vel diam dolor.</p>
+    <f7-navbar title="产品详情" back-link="Back" sliding></f7-navbar>
+
+    <f7-block inner tabs class="content-block-top">
+      <f7-link href="/profit/">保险利益</f7-link>
+      <f7-link href="">条款全文</f7-link>
+      <f7-link href="">投保规则</f7-link>
     </f7-block>
+
+
+    <f7-block inner>
+      222
+    </f7-block>
+    1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>1<br>
   </f7-page>
 </template>
 
@@ -14,4 +20,13 @@
 </script>
 
 <style>
+  .content-block {
+    margin: 10px 0;
+    padding: 0 15px;
+    color: #6d6d72;
+    box-sizing: border-box;
+  }
+  .content-block-top {
+    margin-top: -2px
+  }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 export default [
   {
-    path: '/product-detail/:id/',
-    commponent: require('./pages/product-detail.vue')
+    path: '/product-detail/:id/',  // 产品详情
+    component: require('./pages/product-detail.vue')
+  },
+  {
+    path: '/profit/',  // 保险利益
+    component: require('./pages/profit.vue')
   },
   {
     path: '/about/',
