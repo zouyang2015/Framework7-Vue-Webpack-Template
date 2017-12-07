3cdded7c643e19b58d0f6fe2eb551b152af4b1f0
zouyang@baokeyun.com <86022310@qq.com>
2017-12-07 22:54:33 +0800

    12-7-2

--- src/pages/product-detail.vue
+++ src/pages/product-detail.vue
@@ -13,46 +13,56 @@
       </div>
     </f7-block>
 
+      <f7-list form>
+          <f7-list-item radio name="my-radio" value="1" title="Radio 1" checked></f7-list-item>
+          <f7-list-item radio name="my-radio" value="2" title="Radio 2"></f7-list-item>
+          <f7-list-item radio name="my-radio" value="3" title="Radio 3"></f7-list-item>
+      </f7-list>
+
     <f7-block inner>
       <h2 class="h2-title">被保人信息</h2>
-      <!--<f7-list form>-->
-        <!--<f7-list-item radio name="my-radio" value="1" title="Radio 1" checked></f7-list-item>-->
-        <!--<f7-list-item radio name="my-radio" value="2" title="Radio 2"></f7-list-item>-->
+      <f7-list form>
 
-        <!--&lt;!&ndash; Text Input &ndash;&gt;-->
-        <!--<f7-list-item>-->
-          <!--<f7-label>Name</f7-label>-->
-          <!--<f7-input type="text" placeholder="Name"/>-->
-        <!--</f7-list-item>-->
-        <!--&lt;!&ndash; Password &ndash;&gt;-->
-        <!--<f7-list-item>-->
-          <!--<f7-label>Name</f7-label>-->
-          <!--<f7-input type="password" placeholder="Password"/>-->
-        <!--</f7-list-item>-->
-        <!--&lt;!&ndash; Select &ndash;&gt;-->
-        <!--<f7-list-item>-->
-          <!--<f7-label>Gender</f7-label>-->
-          <!--<f7-input type="select">-->
-            <!--<option value="1">Male</option>-->
-            <!--<option value="1">Female</option>-->
-          <!--</f7-input>-->
-        <!--</f7-list-item>-->
-        <!--&lt;!&ndash; Switch &ndash;&gt;-->
-        <!--<f7-list-item>-->
-          <!--<f7-label>Switch</f7-label>-->
-          <!--<f7-input type="switch" @change="onChange"></f7-input>-->
-        <!--</f7-list-item>-->
-        <!--&lt;!&ndash; Range &ndash;&gt;-->
-        <!--<f7-list-item>-->
-          <!--<f7-label>Range</f7-label>-->
-          <!--<f7-input type="range" min="0" max="100" step="1" value="90"></f7-input>-->
-        <!--</f7-list-item>-->
-        <!--&lt;!&ndash; Textarea &ndash;&gt;-->
-        <!--<f7-list-item>-->
-          <!--<f7-label>Textarea</f7-label>-->
-          <!--<f7-input type="textarea" placeholder="Textarea"></f7-input>-->
-        <!--</f7-list-item>-->
-      <!--</f7-list>-->
+          <f7-list-item checkbox name="my-checkbox" value="1" title="Checkbox 1" checked></f7-list-item>
+          <f7-list-item checkbox name="my-checkbox" value="2" title="Checkbox 1"></f7-list-item>
+
+          <f7-list-item radio name="my-radio" value="1" title="Radio 1" checked></f7-list-item>
+          <f7-list-item radio name="my-radio" value="2" title="Radio 2"></f7-list-item>
+
+        <!-- Text Input -->
+        <f7-list-item>
+          <f7-label>Name</f7-label>
+          <f7-input type="text" placeholder="Name"/>
+        </f7-list-item>
+        <!-- Password -->
+        <f7-list-item>
+          <f7-label>Name</f7-label>
+          <f7-input type="password" placeholder="Password"/>
+        </f7-list-item>
+        <!-- Select -->
+        <f7-list-item>
+          <f7-label>Gender</f7-label>
+          <f7-input type="select">
+            <option value="1">Male</option>
+            <option value="1">Female</option>
+          </f7-input>
+        </f7-list-item>
+        <!-- Switch -->
+        <f7-list-item>
+          <f7-label>Switch</f7-label>
+          <f7-input type="switch" @change="onChange"></f7-input>
+        </f7-list-item>
+        <!-- Range -->
+        <f7-list-item>
+          <f7-label>Range</f7-label>
+          <f7-input type="range" min="0" max="100" step="1" value="90"></f7-input>
+        </f7-list-item>
+        <!-- Textarea -->
+        <f7-list-item>
+          <f7-label>Textarea</f7-label>
+          <f7-input type="textarea" placeholder="Textarea"></f7-input>
+        </f7-list-item>
+      </f7-list>
     </f7-block>
 
 
